01484d7e86e44cae541e2ef7182f1a9d1223f279
maowenbrad <matthew.o.braddy@gmail.com>
2024-02-08 09:11:19 -0600

    feat: add catppuccin theme

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavor = "mocha",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
